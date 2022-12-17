cc8357016923d709106c8cfc64edd375b57de44e
Sidharth Sudhir <sidharthsudhir12@gmail.com>
2022-12-17 11:05:44 +0530

    added login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-
+import 'package:face_off/ui/screens/login_screen.dart';
 import 'package:face_off/ui/screens/profile_complete_page.dart';
 import 'package:face_off/ui/screens/create_profile_1.dart';
 import 'package:face_off/ui/screens/question_page.dart';
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const ProfileCompletePage(),
+      home: const LoginPage(),
  
     );
   }
