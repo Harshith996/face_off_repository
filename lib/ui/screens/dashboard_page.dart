18cc38605fb17332df500fbf10c360d78268b7a4
Harshith Senthilkumaran <harshiths2002@gmail.com>
2022-12-21 12:25:21 +0530

    Only Navbar left

--- lib/ui/screens/dashboard_page.dart
+++ lib/ui/screens/dashboard_page.dart
@@ -1,6 +1,7 @@
 import 'package:face_off/ui/shared/widgets/dangerous_binary_option_buttons.dart';
 import 'package:face_off/ui/shared/widgets/divider.dart';
 import 'package:face_off/ui/widgets/dashboard_page/active_chat.dart';
+import 'package:face_off/ui/widgets/dashboard_page/revealed_chat_line_item.dart';
 import 'package:face_off/utils/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -47,8 +48,8 @@ class _DashboardPageState extends State<DashboardPage> {
         padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Align(
+          children: [
+            const Align(
               alignment: Alignment.centerLeft,
               child: Text(
                 'Anonymous Chats',
@@ -59,26 +60,84 @@ class _DashboardPageState extends State<DashboardPage> {
                 ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 40,
             ),
-            ActiveChat(
+            const ActiveChat(
                 name: "Anonymous 1",
                 description: "typing...",
                 timeLeft: '24h Left',
                 active: true,
                 imgURL: 'assets/images/profile_pictures/anonymous_icon_1.jpg'),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
-            Align(
+            const Align(
                 alignment: Alignment.centerLeft,
                 child: DangerousBinaryOptionButtons(
                     text1: "Request a Faceoff", text2: "Abandon Chat")),
-            SizedBox(
+            const SizedBox(
               height: 30,
             ),
-            LineDivider(length: 220),
+            const LineDivider(length: 220),
+            const SizedBox(
+              height: 30,
+            ),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Revealed Chats',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Color(CustomColors.white),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Revealed chats will expire in 48 hours if a meetup isn\'t scheduled',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color(CustomColors.white),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+                child: SizedBox(
+              height: 150,
+              child: ListView(
+                children: const [
+                  RevealedChatLineItem(
+                      name: "Aubrey Lin",
+                      description: "I would love to meet up with you!",
+                      timeDiff: '19h ago',
+                      imgURL:
+                          'assets/images/profile_pictures/anonymous_icon_5.jpg'),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  RevealedChatLineItem(
+                      name: "Samantha Lee",
+                      description:
+                          "You seem great but I am not really looking to ",
+                      timeDiff: '32h ago',
+                      imgURL:
+                          'assets/images/profile_pictures/anonymous_icon_2.jpg'),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ))
           ],
         ),
       ))),
