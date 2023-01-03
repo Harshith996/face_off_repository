61ac5a5c95c83df3b8fe28a36dd398d78df9bed0
siddhantpaliwal2 <paliwal.siddhant@gmail.com>
2023-01-03 17:52:41 +0530

    Removed Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,12 @@ import 'package:face_off/ui/screens/signup_page.dart';
 import 'package:face_off/ui/screens/splash_screen.dart';
 import 'package:face_off/utils/shared_prefs.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPrefs().init();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  
   runApp(const MyApp());
 }
 
--- lib/ui/shared/widgets/dangerous_binary_option_buttons.dart
+++ lib/ui/shared/widgets/dangerous_binary_option_buttons.dart
@@ -13,7 +13,7 @@ class DangerousBinaryOptionButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return (Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
