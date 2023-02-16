527ee5443fcdb8498f70bd2476ff0b8f2de4b7e9
Sidharth Sudhir <sidharthsudhir12@gmail.com>
2023-02-15 16:46:56 -0800

    fixed camera screen

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,6 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: QuestionOnePage());
+        home: takePic());
   }
 }
